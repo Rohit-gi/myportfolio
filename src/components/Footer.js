9aff54d51afc6ea577b58b3bcda12c769632d946
Rohit <i.rohit003@gmail.com>
2024-03-02 10:10:51 -0600

    Changed year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
         <FacebookIcon />
         <LinkedInIcon />
       </div>
-      <p> &copy; 2023 Rohit's Frontend Portfolio </p>
+      <p> &copy; 2024 Rohit's Frontend Portfolio </p>
     </div>
   );
 }
